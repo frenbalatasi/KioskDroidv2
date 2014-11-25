e6f5eab563aa44109352ab25efa83753b885e631
frenbalatasi <ardaakcay@gmail.com>
2014-11-25 01:03:57 +0100

    Second commit.

--- src/at/ejbox/kioskdroidv2/Main.java
+++ src/at/ejbox/kioskdroidv2/Main.java
@@ -100,6 +100,9 @@ public class Main extends Activity{
 		});
 	}
 
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
